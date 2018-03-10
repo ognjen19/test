2ae560921057273fcb49de5097dece2faf369a95
ognjen <ognjen@Ognjen>
2018-03-10 14:27:52 +0100

    fix

--- src/ognjen/stankovic/Main.java
+++ src/ognjen/stankovic/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-
 		System.out.println("hello world ogi");
+		
+		System.out.println("con1");
+		
+		System.out.println("con2");
 		
 	}
 
